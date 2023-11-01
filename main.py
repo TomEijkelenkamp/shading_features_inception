68b8e9f68c80d53b33e17c4b48ff5bebff656e18
TomEijkelenkamp <66938518+TomEijkelenkamp@users.noreply.github.com>
2023-11-01 10:54:34 +0100

    working

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from tqdm import tqdm
+import pandas as pd
 import os
 import re
-import pandas as pd
 import tensorflow as tf
 from scipy.stats import pearsonr
-import tqdm
 
 # Load the Inception model
 model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet', pooling='avg')
@@ -16,8 +16,8 @@ def preprocess_image(image_path):
     img_array = tf.keras.applications.inception_v3.preprocess_input(img_array)
     return img_array
 
-# Initialize an empty DataFrame to store metadata
-metadata = pd.DataFrame(columns=['shape', 'color_index', 'metallic_index', 'roughness_index', 'rotation_angle_index', 'light_position_index', 'file_name'])
+# Initialize a list to store metadata
+metadata_list = []
 
 # Regular expression to parse the filename
 pattern = re.compile(r'render_(\w+)_c(\d+)_m(\d+)_r(\d+)_a(\d+)_l(\d+)')
@@ -31,7 +31,7 @@ for img_name in tqdm(os.listdir(image_folder), desc="Processing Images", unit="i
     img_path = os.path.join(image_folder, img_name)
     match = pattern.match(img_name)
     if match:
-        metadata = metadata.append({
+        metadata_list.append({
             'shape': match.group(1),
             'color_index': int(match.group(2)),
             'metallic_index': int(match.group(3)),
@@ -39,21 +39,31 @@ for img_name in tqdm(os.listdir(image_folder), desc="Processing Images", unit="i
             'rotation_angle_index': int(match.group(5)),
             'light_position_index': int(match.group(6)),
             'file_name': img_name
-        }, ignore_index=True)
+        })
         
         img_array = preprocess_image(img_path)
         img_features = model.predict(img_array)
         features.append(img_features[0])
-        
+
+# Convert metadata list to DataFrame
+metadata = pd.DataFrame(metadata_list)
+
+# Convert categorical 'shape' column to numerical form
+metadata = pd.get_dummies(metadata, columns=['shape'])
+
 # Convert feature list to DataFrame
 features_df = pd.DataFrame(features)
 
-# Calculate correlation
-correlations = pd.DataFrame(index=metadata.columns[:-1], columns=features_df.columns)
-for meta_col in metadata.columns[:-1]:  # Exclude 'file_name'
-    for feature_col in features_df.columns:
-        correlation, _ = pearsonr(metadata[meta_col], features_df[feature_col])
-        correlations.loc[meta_col, feature_col] = correlation
+# Calculate correlation only for numeric data
+correlations = pd.DataFrame(index=metadata.columns[:-1], columns=features_df.columns)  # Exclude 'file_name'
+for meta_col in metadata.columns[:-1]:
+    if pd.api.types.is_numeric_dtype(metadata[meta_col]):
+        for feature_col in features_df.columns:
+            correlation, _ = pearsonr(metadata[meta_col], features_df[feature_col])
+            correlations.loc[meta_col, feature_col] = correlation
 
 # Print correlations
 print(correlations)
+
+# Save correlations to CSV file
+correlations.to_csv('shading_feature_correlations.csv', sep=';')
